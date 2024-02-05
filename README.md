b72bb7234a375b77898191f196962ab6c08a4f00
Nayak Omkar <nayaksomkar+github.dev@outlook.in>
2024-02-05 15:02:02 +0530

    cxc vx

--- README.md
+++
@@ -1,2 +0,0 @@
-# TrackRabbit
-Todo app with notification access, pings you again and again to remeber you and frequency can be adjusted.
--- android/app/src/main/kotlin/com/example/fluttertemplate/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fluttertemplate
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
